090b7a31924f1c1f2f0864091083858d99837c38
Gagan Yarramsetty <gaganyarramsetty@gmail.com>
2026-02-17 15:40:04 +0530

    sequential part completed

--- bin/master.rs
+++ bin/master.rs
@@ -1,17 +1,26 @@
-use std::{io::BufReader, net::TcpListener};
+use std::{
+    io::{BufReader, Write},
+    net::TcpListener,
+    sync::Mutex,
+};
 
-use mapreduce::rpc::{Request, Response};
+use mapreduce::{master::Master, rpc::Request};
 
 pub fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let input_files = vec!["input/file1.txt".to_string(), "input/file2.txt".to_string()];
+    let n_reduce = 5;
+    let output_path = "output".to_string();
+    let master = Mutex::new(Master::new(input_files, n_reduce, output_path));
     let listener = TcpListener::bind("127.0.0.1:8080")?;
+    println!("master listening on 127.0.0.1:8080");
     for stream in listener.incoming() {
-        let stream = stream?;
-        let buf = BufReader::new(&stream);
+        let mut stream = &stream?;
+        let buf = BufReader::new(stream);
         let req: Request = serde_json::from_reader(buf)?;
-        println!("we Go request ra {:?}", req);
-        // --- process ----
-        let resp = Response::NoTask;
+        let mut master = master.lock().unwrap();
+        let resp = master.handle_request(req);
         serde_json::to_writer(stream, &resp)?;
+        stream.flush()?;
     }
     Ok(())
 }
